17ad044304de918bed6f1e29f200249e11dff8f5
silentbob714 <74814333+silentbob714@users.noreply.github.com>
2026-07-07 04:10:40 -0700

    Update opensky_test.py

--- opensky_test.py
+++ opensky_test.py
@@ -1,15 +1,25 @@
 import os
 import requests
 
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
+
 username = os.environ["OPENSKY_USERNAME"]
 password = os.environ["OPENSKY_PASSWORD"]
 
-url = "https://opensky-network.org/api/states/all"
+try:
+    response = requests.get(
+        "https://opensky-network.org/api/states/all",
+        auth=(username, password),
+        timeout=120
+    )
 
-response = requests.get(
-    url,
-    auth=(username, password),
-    timeout=30
-)
+    payload = {
+        "content": f"Status: {response.status_code}"
+    }
 
-print(response.status_code)
+except Exception as e:
+    payload = {
+        "content": f"ERROR: {type(e).__name__}: {str(e)}"
+    }
+
+requests.post(WEBHOOK, json=payload)
